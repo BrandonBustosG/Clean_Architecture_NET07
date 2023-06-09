4852458a281662a31e685a69a5fa89e97bf08c49
Brandon.Bustos <brandon.bustos@olsoftware.com>
2023-06-09 18:42:58 -0500

    feat: :sparkles: se agrega servicios a la capa aplicacion

---
+++ Application/Data/IApplicationDbContext.cs
@@ -0,0 +1,10 @@
+using Domain.Customers;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Data;
+
+public interface IApplicationDbContext
+{
+    DbSet<Customer> Customers { get; set; }
+    Task<int> SaveChangeAsync(CancellationToken cancellationToken = default);
+}
