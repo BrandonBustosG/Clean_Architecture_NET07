33c56322d3bab6f6e447b329d4268aac51583e76
Brandon.Bustos <brandon.bustos@olsoftware.com>
2023-07-18 18:25:35 -0500

    feat: agregando validation behavior

--- Application/DependencyInjection.cs
+++ Application/DependencyInjection.cs
@@ -1,4 +1,6 @@
+using Application.Common.Behaviors;
 using FluentValidation;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Application;
@@ -8,6 +10,12 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddMediatR(config => config.RegisterServicesFromAssemblyContaining<ApplicationAssemblyReference>());
+
+        services.AddScoped(
+            typeof(IPipelineBehavior<,>),
+            typeof(ValidationBehavior<,>)
+            );
+
         services.AddValidatorsFromAssemblyContaining<ApplicationAssemblyReference>();
         return services;
     }
