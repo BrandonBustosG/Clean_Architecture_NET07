3a4be9b2e945b520e0a0a937926852efda6246fb
Brandon.Bustos <brandon.bustos@olsoftware.com>
2023-07-28 17:05:29 -0500

    feat: agregando middleware global

--- Application/Customers/Create/CreateCustomerCommandHandler.cs
+++ Application/Customers/Create/CreateCustomerCommandHandler.cs
@@ -6,47 +6,40 @@ using MediatR;
 
 namespace Application.Customers.Create;
 
-internal sealed class CreateCustomerCommandHandler : IRequestHandler<CreateCustomerCommand, ErrorOr<Unit>>
+public sealed class CreateCustomerCommandHandler : IRequestHandler<CreateCustomerCommand, ErrorOr<Unit>>
 {
-    private readonly ICustomerRepository _customerRepository;
-    private readonly IUnitOfWork _unitOfWork;
+	private readonly ICustomerRepository _customerRepository;
+	private readonly IUnitOfWork _unitOfWork;
 
-    public CreateCustomerCommandHandler(ICustomerRepository customerRepository, IUnitOfWork unitOfWork)
-    {
-        _customerRepository = customerRepository ?? throw new ArgumentNullException(nameof(customerRepository));
-        _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
-    }
+	public CreateCustomerCommandHandler(ICustomerRepository customerRepository, IUnitOfWork unitOfWork)
+	{
+		_customerRepository = customerRepository ?? throw new ArgumentNullException(nameof(customerRepository));
+		_unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+	}
 
-    public async Task<ErrorOr<Unit>> Handle(CreateCustomerCommand command, CancellationToken cancellationToken)
-    {
-        try
-        {
-            if (PhoneNumber.Create(command.PhoneNumber) is not PhoneNumber phoneNumber)
-            {
-                return Error.Validation("Customer.Phonenumber", "Phone number has not valid format");
-            }
-            if (Address.Create(command.Country, command.Line1, command.Line2, command.City, command.State, command.ZipCode) is not Address address)
-            {
-                return Error.Validation("Customer.Address", "Address is not valid");
-            }
+	public async Task<ErrorOr<Unit>> Handle(CreateCustomerCommand command, CancellationToken cancellationToken)
+	{
 
-            var customer = new Customer(
-                new CustomerId(Guid.NewGuid()),
-                command.Name,
-                command.LastName,
-                command.Email,
-                phoneNumber,
-                address,
-                true
-            );
-            await _customerRepository.Add(customer);
-            await _unitOfWork.SaveChangesAsync(cancellationToken);
-            return Unit.Value;
-        }
-        catch (Exception ex)
-        {
+		if (PhoneNumber.Create(command.PhoneNumber) is not PhoneNumber phoneNumber)
+		{
+			return Error.Validation("Customer.Phonenumber", "Phone number has not valid format");
+		}
+		if (Address.Create(command.Country, command.Line1, command.Line2, command.City, command.State, command.ZipCode) is not Address address)
+		{
+			return Error.Validation("Customer.Address", "Address is not valid");
+		}
 
-            return Error.Failure("CreateCustomer.Failure", ex.Message);
-        }
-    }
+		var customer = new Customer(
+			new CustomerId(Guid.NewGuid()),
+			command.Name,
+			command.LastName,
+			command.Email,
+			phoneNumber,
+			address,
+			true
+		);
+		 _customerRepository.Add(customer);
+		await _unitOfWork.SaveChangesAsync(cancellationToken);
+		return Unit.Value;
+	}
 }
